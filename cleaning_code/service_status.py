18857259305b66d1bbe8c859279310b7664a0be5
Paul Horn <knutwalker@gmail.com>
2012-09-16 17:35:54 +0200

    update status.xml to serviceStatus.txt

--- cleaning_code/service_status.py
+++ cleaning_code/service_status.py
@@ -1,8 +1,11 @@
+"""
+Download from http://www.mta.info/status/serviceStatus.txt
+"""
 import json
 import xmltodict
 
 # the xml file is available at http://www.mta.info/status/serviceStatus.txt
-s = open("code/status.xml").read()
+s = open("serviceStatus.txt").read()
 # we convert it to a dictionary
 d = xmltodict.parse(s)
 # and then dump the subway section into a JSON file to visualise
