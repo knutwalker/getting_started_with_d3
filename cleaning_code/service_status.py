a8d0e468db74fb7572563ce23a81c07abf3bb5b5
Paul Horn <knutwalker@gmail.com>
2012-09-17 20:40:16 +0200

    Fix service_status to work with new xmltodict version

--- cleaning_code/service_status.py
+++ cleaning_code/service_status.py
@@ -1,9 +1,9 @@
 import json
-import xmltodict as xml
+import xmltodict
 
 # the xml file is available at http://www.mta.info/status/serviceStatus.txt
 s = open("code/status.xml").read()
 # we convert it to a dictionary
-d = xml.xmltodict(s)
+d = xmltodict.parse(s)
 # and then dump the subway section into a JSON file to visualise
 json.dump(d['service']['subway']['line'], open("../visualisations/data/service_status.json",'w'))
